9228bb58ed2bacf70adcdd87c028c8919c3ab9b6
negier <1208883805@qq.com>
2021-06-11 15:44:58 +0800

    fix something wrong

--- view_binding/src/main/java/com/xuebinduan/view_binding/BlankFragment.kt
+++ view_binding/src/main/java/com/xuebinduan/view_binding/BlankFragment.kt
@@ -9,21 +9,14 @@ import com.xuebinduan.view_binding.databinding.FragmentBlankBinding
 
 class BlankFragment : Fragment() {
 
-    private var _binding: FragmentBlankBinding? = null
-    private val binding: FragmentBlankBinding = _binding!!
+    private lateinit var binding: FragmentBlankBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentBlankBinding.inflate(inflater, container, false)
+        binding = FragmentBlankBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
     }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
 }
